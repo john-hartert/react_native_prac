a7d802e6358d37f5ef6c75b0c8c0d913c418bcc0
John Hartert <j.blainehartert@gmail.com>
2017-09-27 15:18:01 -0400

    More comments and imports react-native and react

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,1 +1,11 @@
 // Index.ios.js - place code here for IOS!!!!!
+// Use https://facebook.github.io/react-native/docs/getting-started.html
+// npm install -g create-react-native-app
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+
+// Render it to the device
