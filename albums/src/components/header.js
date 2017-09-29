553a5c47526a60b1d0606ef54cd2697e5bcb04c6
John Hartert <j.blainehartert@gmail.com>
2017-09-29 10:11:58 -0400

    New components folder and starting header component

---
+++ albums/src/components/header.js
@@ -0,0 +1,10 @@
+// Import libraries for making a component.
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Make a component
+const Header = () => {
+    return <Text></Text>;
+};
+
+// Make the component available to other parts of the app.
